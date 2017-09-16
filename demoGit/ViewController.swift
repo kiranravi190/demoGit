a21bcb3acf38ce77bab24081ce17ca30606855bb
ravikiran bonthala <Ravikiran@ravikirans-macbook-pro.local>
2017-09-16 23:18:38 +0530

    update

--- demoGit/ViewController.swift
+++ demoGit/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         print("hello")
         print("branch1 commit")
 
+        print("mster commit")
     }
 
     override func didReceiveMemoryWarning() {
