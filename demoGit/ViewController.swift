2c02529bd672d35054138c77c366ee691c359911
ravikiran bonthala <Ravikiran@ravikirans-macbook-pro.local>
2017-09-16 23:16:20 +0530

    branch1 updtate

--- demoGit/ViewController.swift
+++ demoGit/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("hello")
+        print("branch1 commit")
+
     }
 
     override func didReceiveMemoryWarning() {
