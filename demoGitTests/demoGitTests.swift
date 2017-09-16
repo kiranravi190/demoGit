ec1558ce8e97e904cd0b7f52bb5d2bad33dbdb4c
ravikiran bonthala <Ravikiran@ravikirans-macbook-pro.local>
2017-09-16 23:08:41 +0530

    Initial Commit

---
+++ demoGitTests/demoGitTests.swift
@@ -0,0 +1,36 @@
+//
+//  demoGitTests.swift
+//  demoGitTests
+//
+//  Created by Ravi Bonthala on 16/09/17.
+//  Copyright © 2017 Ravi Bonthala. All rights reserved.
+//
+
+import XCTest
+@testable import demoGit
+
+class demoGitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
