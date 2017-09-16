73649df3300ced517e897883cf434df3c2e091ac
Ravi Bonthala <kiranravi190@gmail.com>
2017-09-17 00:00:01 +0530

    no message

--- demoGit/AppDelegate.swift
+++ demoGit/AppDelegate.swift
@@ -16,7 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+        print("mster commit")
+        print("mster commit")
+        print("mster commit")
+        print("mster commit")
+        print("mster commit")
+
          print("mster commit")
         print("mster commit")
 
